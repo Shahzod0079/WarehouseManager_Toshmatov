c7862a7af267a6a88dd76f74005da65d51543c86
student-v424 <student-v424@STUD-RM6-V424>
2026-03-28 10:53:26 +0300

    Создание модель представления VM_Pages. Тошматов

---
+++ WarehouseManager_Toshmatov/ViewModels/VM_pages.cs
@@ -0,0 +1,26 @@
+﻿using WarehouseManager_Toshmatov.Classes;
+using WarehouseManager_Toshmatov.Views;
+
+namespace WarehouseManager_Toshmatov.ViewModels
+{
+    public class VM_Pages : Notification
+    {
+        public VM_Warehouse vm_Warehouse = new VM_Warehouse();
+
+        public VM_Pages()
+        {
+            MainWindow.init.frame.Navigate(new Main(vm_Warehouse));
+        }
+
+        public RealyCommand OnClose
+        {
+            get
+            {
+                return new RealyCommand(obj =>
+                {
+                    MainWindow.init.Close();
+                });
+            }
+        }
+    }
+}
