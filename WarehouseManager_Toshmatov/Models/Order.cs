1a9b2cdfa5fb6c6894e9f16f27553437d9950cfd
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-28 10:34:09 +0500

    Создание модели данных Suplier и Order. Тошматов

---
+++ WarehouseManager_Toshmatov/Models/Order.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using WarehouseManager_Toshmatov.Classes;
+
+namespace WarehouseManager_Toshmatov.Models
+{
+    public class Order : Notification
+    {
+        public int Id { get; set; }
+
+        private string productName;
+        public string ProductName
+        {
+            get { return productName; }
+            set
+            {
+                productName = value;
+                OnPropertyChanged("ProductName");
+            }
+        }
+
+        private int quantity;
+        public int Quantity
+        {
+            get { return quantity; }
+            set
+            {
+                quantity = value;
+                OnPropertyChanged("Quantity");
+            }
+        }
+        private decimal price;
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+                OnPropertyChanged("TotalAmout");
+            }
+        }
+
+        private DateTime orderDate;
+        public DateTime OrderDate
+        {
+            get { return orderDate; }
+            set
+            {
+                orderDate = value;
+                OnPropertyChanged("OrderDate");
+            }
+        }
+
+        public decimal TotalAmount
+        {
+            get { return Quantity * Price;  }
+        }
+
+        private bool isCompleted
+        public bool IsCompleted
+        {
+            get { return isCompleted; }
+            set
+            {
+                isCompleted = value;
+                OnPropertyChanged("isCompleted");
+                OnPropertyChanged("StatusText");
+            }
+        }
+
+        public string StatusText
+        {
+            get { return IsCompleted ? "Выполнен" : "В обработке"; }
+        }
+
+        private int? supplierId;
+        public int? SupplierId
+        {
+            get { return supplierId; }
+            set
+            {
+                supplierId = value;
+                OnPropertyChanged("SupplierId");
+            }
+        }
+
+        private Supplier supplier;
+        public Supplier Supplier
+        {
+            get { return supplier; }
+            set
+            {
+                supplier = value;
+                OnPropertyChanged("Supplier");
+            }
+        }
+
+    }
+}
---
+++ WarehouseManager_Toshmatov/Models/Supplier.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarehouseManager_Toshmatov.Classes;
+
+namespace WarehouseManager_Toshmatov.Models
+{
+    public class Supplier : Notification
+    {
+        public int Id { get; set; }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("ContactPerson");
+            }
+        }
+        private string phone;
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+                OnPropertyChanged("Phone");
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+        public virtual ObservableCollection<Order> Orders { get; set; }
+
+        public Supplier()
+        {
+            Orders = new ObservableCollection<Order>();
+        }
+    }
+}
