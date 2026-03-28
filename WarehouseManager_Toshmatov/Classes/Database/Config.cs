b33eed7ca43634f653df9fd2312422b412725679
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-28 10:13:12 +0500

    Создание классов Config и Notification. Тошматов

---
+++ WarehouseManager_Toshmatov/Classes/Database/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManager_Toshmatov.Classes.Database
+{
+    public class Config
+    {
+        public static string ConnectionConfig = @"Server=(localdb)\mssqllocaldb;Database=WarehouseDB;Trusted_Connection=True;";
+    }
+}
---
+++ WarehouseManager_Toshmatov/Classes/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WarehouseManager_Toshmatov.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+
