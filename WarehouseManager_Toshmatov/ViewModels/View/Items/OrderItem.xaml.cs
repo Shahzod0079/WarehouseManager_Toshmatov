af4af586db80ad4ff46d5462d3ed463c218a51ff
Shahzod Toshmatov <toshmatovshahzod69@gmail.com>
2026-03-28 17:26:21 +0500

    Создание пользовательский элемент OrderItem. Тошматов

---
+++ WarehouseManager_Toshmatov/ViewModels/View/Items/OrderItem.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WarehouseManager_Toshmatov.Views.Items
+{
+    public partial class OrderItem : UserControl
+    {
+        public OrderItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
